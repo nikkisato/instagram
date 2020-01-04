045b434fb3337a1d725b7eb7c1d3163e31a4ee95
Nikki <nikki.satopdx@gmail.com>
2020-01-03 16:44:29 -0800

    added slider but it isn't working

---
+++ app.js
@@ -0,0 +1,36 @@
+$('body').toggleClass(localStorage.toggled);
+
+function darkLight() {
+    if (localStorage.toggled != 'dark') {
+        $('body, p').toggleClass('dark', true);
+        localStorage.toggled = 'dark';
+    } else {
+        $('body, p').toggleClass('dark', false);
+        localStorage.toggled = '';
+    }
+}
+
+if ($('body').hasClass('dark')) {
+    $('#checkBox').prop('checked', true);
+} else {
+    $('#checkBox').prop('checked', false);
+}
+
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+});
+          
+       
