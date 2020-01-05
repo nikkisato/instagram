56243b5e026ed485b87539f7e964299f2e8c4a9d
Nikki <nikki.satopdx@gmail.com>
2020-01-05 15:04:16 -0800

    finished:

--- app.js
+++ app.js
@@ -16,20 +16,3 @@ if ($('body').hasClass('dark')) {
     $('#checkBox').prop('checked', false);
 }
 
-$('.slider-for').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    arrows: false,
-    fade: true,
-    asNavFor: '.slider-nav'
-});
-$('.slider-nav').slick({
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    asNavFor: '.slider-for',
-    dots: true,
-    centerMode: true,
-    focusOnSelect: true
-});
-          
-       
